951ab7d91d974fba21d0808454daf4a401e31fc5
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2022-09-03 07:13:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,5 +28,5 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 #添加smartdns
 #git clone --depth=1 https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
 
-#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-#git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
