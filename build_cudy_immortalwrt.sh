05d588fcf4fcb45fb7332ab264fd33c24ce57dea
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2026-03-13 21:55:04 +0800

    ath79: disable build for fortinet fap-220-b by default

--- build_cudy_immortalwrt.sh
+++ build_cudy_immortalwrt.sh
@@ -8,8 +8,6 @@ SRC_DIR="immortalwrt"
 CONFIG_FILE="cudy.config"
 PATCH_FILE="cudy.patch"
 
-git reset --hard bbe3e58df01476a499ffcdc1098a418946d368b0 # ath79: disable build for fortinet fap-220-b by default
-
 
 require_file() {
     if [ ! -f "$1" ]; then
@@ -33,6 +31,8 @@ require_file "$PATCH_FILE"
 
 if [ ! -d "$SRC_DIR/.git" ]; then
     git clone "$REPO_URL" -b "$BRANCH" "$SRC_DIR"
+    
+    git reset --hard bbe3e58df01476a499ffcdc1098a418946d368b0 # ath79: disable build for fortinet fap-220-b by default
 else
     echo "Using existing source tree: $SRC_DIR"
 fi
