f4be6f54526345ef852dc437fb1e90e3ea3a5f63
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2026-03-19 19:15:21 +0800

    Merge Official Source

--- build_cudy_immortalwrt.sh
+++ build_cudy_immortalwrt.sh
@@ -33,7 +33,7 @@ if [ ! -d "$SRC_DIR/.git" ]; then
     git clone "$REPO_URL" -b "$BRANCH" "$SRC_DIR"
     (
       cd $SRC_DIR
-      git reset --hard 62d8101e09042f15c4770d59fdda1d16158a511a # tools/expat: fix PKG_CPE_ID
+      git reset --hard 1cc0390ddf5453691059dc4d21e8d1ca3277e023 # Merge Official Source
     )
 else
     echo "Using existing source tree: $SRC_DIR"
